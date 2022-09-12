d277ae671f6fb2e91e75f2b0aa7919e91ff6f418
Balint Horvath <horvath.balint997@gmail.com>
2022-09-12 17:45:42 +0200

    debug

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];           // elmaradt "_"
+    std::cout << "1-100 ertekek duplazasa"; // elmaradt ";", idézőjel ' helyett
+    for (int i = 0; i <= N_ELEMENTS; i++)   // hiányos ciklus vezérlő
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i <= N_ELEMENTS; i++) // hibás ciklus vezérlő
     {
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek:"; // elmaradt ";"
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;                       // kell kezdőérték
+    for (int i = 0; i < N_ELEMENTS; i++) // , helyett ; kell
     {
-        atlag += b[i]
+        atlag += b[i]; // elmaradt ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
